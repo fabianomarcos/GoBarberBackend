ee63b2ed1864fda28a055ac0b8219820f0fc7cb5
Fabiano Marcos <fabiano@vexter.com.br>
2020-09-16 23:08:36 -0300

    base project

---
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -0,0 +1,35 @@
+import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
+import AppError from '../../../shared/errors/AppError';
+
+interface Request {
+  provider_id: any;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
+
+    if(findAppointmentInSameDate) {
+      throw new AppError('Este horário está indisponível.', 400);
+    }
+
+    const appointment = appointmentsRepository.create({
+      provider_id,
+      date: appointmentDate
+    });
+
+    await appointmentsRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
---
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -0,0 +1,44 @@
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
+import ensureAuthenticated from '../middleware/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+
+  const createUser = new CreateUserService();
+
+  const user = await createUser.execute({
+    name, email, password
+  });
+
+ // delete (await user).password;
+
+  return response.json(user);
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
+
+   // delete (await user).password;
+
+    return response.json(user);
+  }
+);
+
+export default usersRouter;
