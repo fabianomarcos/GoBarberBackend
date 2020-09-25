ca7a8682028485a54b6e84db646be6be3e697cac
Fabiano Marcos <fabiano@vexter.com.br>
2020-09-24 22:52:04 -0300

    professional listings

---
+++ src/modules/users/dtos/IFindAllProvidersDTO.ts
@@ -0,0 +1,5 @@
+import { de } from 'date-fns/locale';
+
+export default interface IFindAllProvidersDTO {
+  except_user_id?: string;
+}
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import profileRouter from '@modules/users/infra/http/routes/profile.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import appointmentsRouter from '../../../../modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '../../../../modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '../../../../modules/users/infra/http/routes/session.routes';
@@ -9,6 +10,7 @@ import passwordRouter from '../../../../modules/users/infra/http/routes/password
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
