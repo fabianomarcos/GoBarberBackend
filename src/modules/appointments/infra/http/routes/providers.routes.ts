715386c2d683cb86d05ec35dc16c0035f1ebec23
Fabiano Marcos <fabiano@vexter.com.br>
2020-09-26 02:31:26 -0300

    routes availability days and month for provider

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -1,3 +1,5 @@
+import ProviderDayAvailabilityController from '@modules/users/infra/http/controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '@modules/users/infra/http/controllers/ProviderMonthAvailabilityController';
 import { Router } from 'express';
 
 import ensureAuthenticated from '../../../../users/infra/http/middleware/ensureAuthenticated';
@@ -5,8 +7,20 @@ import ProvidersController from '../controllers/ProvidersController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 providersRouter.use(ensureAuthenticated);
+
 providersRouter.get('/', providersController.index);
+
+providersRouter.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default providersRouter;
