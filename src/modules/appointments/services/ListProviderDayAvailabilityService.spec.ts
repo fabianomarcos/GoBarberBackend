89221360f5ba4d4eeaac0d48a4a78bc0b5edf015
Fabiano Marcos <fabiano@vexter.com.br>
2020-09-25 23:48:17 -0300

    list of available times for appointments

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -13,13 +13,18 @@ describe('ListProviderDayAvailability', () => {
   });
 
   it('should be able to list the month availability from provider', async () => {
-    const hours = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 14, 0, 0),
+    });
 
-    hours.map(async hour => {
-      await fakeAppointmentsRepository.create({
-        provider_id: 'user',
-        date: new Date(2020, 4, 20, hour - 3, 0, 0),
-      });
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
@@ -30,9 +35,16 @@ describe('ListProviderDayAvailability', () => {
     });
 
     const results = [
-      { available: true, hour: 8 },
-      { available: true, hour: 9 },
-      { available: true, hour: 10 },
+      { available: false, hour: 8 },
+      { available: false, hour: 9 },
+      { available: false, hour: 10 },
+      { available: false, hour: 11 },
+      { available: true, hour: 12 },
+      { available: true, hour: 13 },
+      { available: false, hour: 14 },
+      { available: false, hour: 15 },
+      { available: true, hour: 16 },
+      { available: true, hour: 17 },
     ];
 
     expect(availability).toEqual(expect.arrayContaining(results));
