602d80254f7832b209fd30dc1fe4ca40afe246a4
Fabiano Marcos <fabiano@vexter.com.br>
2020-10-01 18:58:15 -0300

    finally configuration upload image

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -2,6 +2,8 @@ import fs from 'fs';
 import aws, { S3 } from 'aws-sdk';
 import path from 'path';
 import uploadConfig from '@config/upload';
+import mime from 'mime';
+import AppError from '@shared/errors/AppError';
 import IStorageProvider from '../models/IStorageProvider';
 
 class S3StorageProvider implements IStorageProvider {
@@ -16,18 +18,25 @@ class S3StorageProvider implements IStorageProvider {
   public async saveFile(file: string): Promise<string> {
     const originalPath = path.resolve(uploadConfig.tmpFolder, file);
 
-    const fileContent = await fs.promises.readFile(originalPath, {
-      encoding: 'utf-8',
-    });
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new AppError('Arquivo não encontrado.');
+    }
+
+    const fileContent = await fs.promises.readFile(originalPath);
 
     await this.client
       .putObject({
-        Bucket: 'app-gobarber',
+        Bucket: uploadConfig.config.aws.bucket,
         Key: file,
         ACL: 'public-read',
         Body: fileContent,
+        ContentType,
       })
       .promise();
+
+    await fs.promises.unlink(originalPath);
 
     return file;
   }
@@ -35,11 +44,11 @@ class S3StorageProvider implements IStorageProvider {
   public async deleteFile(file: string): Promise<void> {
     await this.client
       .deleteObject({
-        Bucket: 'app-gobarber',
+        Bucket: uploadConfig.config.aws.bucket,
         Key: file,
       })
       .promise();
   }
 }
 
-export default S3StorageProvider; // tempo 22:19
+export default S3StorageProvider;
